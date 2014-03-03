847d1d730a545895d9068903bbaf8f0309ff1c17
Ashley Fernandes <ashley_fernandes@intuit.com>
2014-03-03 10:40:06 -0800

    Initial Commit

---
+++ MoneyMeter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoneyMeter
+//
+//  Created by Fernandes, Ashley on 3/3/14.
+//  Copyright (c) 2014 Intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoneyMeter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MoneyMeter
+//
+//  Created by Fernandes, Ashley on 3/3/14.
+//  Copyright (c) 2014 Intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
